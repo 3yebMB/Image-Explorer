ecf735e2269fd14b44fdab7295beb627fb0b871f
3yebmb <3yebmb@gmail.com>
2021-04-17 10:00:15 +0500

    Add paging

--- app/src/main/java/dev/m13d/imageexplorer/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/m13d/imageexplorer/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,22 @@
 package dev.m13d.imageexplorer.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.m13d.imageexplorer.R
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            
+        }
+    }
 }
