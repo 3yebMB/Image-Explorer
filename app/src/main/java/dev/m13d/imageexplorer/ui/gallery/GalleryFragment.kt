5429b2e6a7a4936a7bea6ef67260377ed56dbb35
3yebmb <3yebmb@gmail.com>
2021-04-17 10:46:39 +0500

    Add PagingDataAdapter

--- app/src/main/java/dev/m13d/imageexplorer/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/m13d/imageexplorer/ui/gallery/GalleryFragment.kt
@@ -6,17 +6,33 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.m13d.imageexplorer.R
+import dev.m13d.imageexplorer.databinding.FragmentGalleryBinding
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
-            
+        _binding = FragmentGalleryBinding.bind(view)
+
+        val adapter = UnsplashPhotoAdapter()
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
